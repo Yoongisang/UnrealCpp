9d7c1ac41bef479db6bb4a52e00faaad72482d54
Yoongisang <sawsd@naver.com>
2025-06-16 22:52:39 +0900

    update

---
+++ Source/UnrealCpp/MyCharacter.cpp
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "MyAnimInstance.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
+
+	if (SM.Succeeded())
+	{
+
+		GetMesh()->SetSkeletalMesh(SM.Object);
+		GetMesh()->SetRelativeLocationAndRotation(FVector(0.f, 0.f, -90.f), FRotator(0.f, -90.f, 0.f));
+	}
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	
+	CameraBoom->SetupAttachment(RootComponent);
+	FollowCamera->SetupAttachment(CameraBoom);
+	CameraBoom->TargetArmLength = 400.0f;
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AnimInstacne = Cast<UMyAnimInstance>(GetMesh()->GetAnimInstance());
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("ForwardBackward"), this, &AMyCharacter::KeyUpdown);
+	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AMyCharacter::KeyLeftright);
+
+	PlayerInputComponent->BindAction(TEXT("Attack"), EInputEvent::IE_Pressed, this, &AMyCharacter::KeyAttack);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AMyCharacter::Jump);
+
+
+}
+
+void AMyCharacter::KeyUpdown(float value)
+{
+	AddMovementInput(GetActorForwardVector(), value, false);
+}
+
+void AMyCharacter::KeyLeftright(float value)
+{
+	AddMovementInput(GetActorRightVector(), value, false);
+
+}
+
+void AMyCharacter::KeyAttack()
+{
+	UE_LOG(LogTemp, Log, TEXT("Attack"));
+}
+
---
+++ Source/UnrealCpp/MyCharacter.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class UNREALCPP_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere);
+	class UAnimInstance* AnimInstacne;
+
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* FollowCamera;
+
+public:
+	// Sets default values for this character's properties
+	AMyCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void KeyUpdown(float value);
+	void KeyLeftright(float value);
+	void KeyAttack();
+};
